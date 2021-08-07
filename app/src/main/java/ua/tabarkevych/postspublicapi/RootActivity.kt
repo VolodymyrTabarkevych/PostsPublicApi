49528ab6d28dd16a1866e11040ac278fcc0b3107
Володимир Табаркевич <w.tabarkevych@gmail.com>
2021-08-07 21:46:36 +0300

    Enable view binding. Setup navigation component. Add posts fragment layout.

--- app/src/main/java/ua/tabarkevych/postspublicapi/RootActivity.kt
+++ app/src/main/java/ua/tabarkevych/postspublicapi/RootActivity.kt
@@ -2,10 +2,14 @@ package ua.tabarkevych.postspublicapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ua.tabarkevych.postspublicapi.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRootBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
