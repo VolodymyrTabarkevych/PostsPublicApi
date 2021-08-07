befb1873cfe0ffbbd9de859dc49a836ebbd60c89
Володимир Табаркевич <w.tabarkevych@gmail.com>
2021-08-07 23:42:10 +0300

    Add hilt entry point and set app name

--- app/src/main/java/ua/tabarkevych/postspublicapi/PostsPublicApiApp.kt
+++ app/src/main/java/ua/tabarkevych/postspublicapi/PostsPublicApiApp.kt
@@ -1,6 +1,7 @@
 package ua.tabarkevych.postspublicapi
 
+import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PostsPublicApiApp
+class PostsPublicApiApp : Application()
--- app/src/main/java/ua/tabarkevych/postspublicapi/RootActivity.kt
+++ app/src/main/java/ua/tabarkevych/postspublicapi/RootActivity.kt
@@ -2,8 +2,10 @@ package ua.tabarkevych.postspublicapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import ua.tabarkevych.postspublicapi.databinding.ActivityRootBinding
 
+@AndroidEntryPoint
 class RootActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRootBinding
 
